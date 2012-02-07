f6ae7a4e9060d3a61db758a3f31e0426196a93e1
jorismeys (MSc. ) <joris.meys@ugent.be>
2012-02-07 16:21:12 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/disco@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,7 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p> This project contains the functions that accompany the book <a href=http://www.springer.com/statistics/book/978-0-387-92709-1>Comparing Distributions by Olivier Thas.</a> </p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
